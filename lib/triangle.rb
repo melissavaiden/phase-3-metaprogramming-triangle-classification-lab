919f8cce68e91b2b8380bbbe80deb2ea2912adab
Melissa Vaiden <melissavaiden@gmail.com>
2022-09-10 14:40:51 -0400

    commit tree 8845ad1c94251b9f7af9bd70ba497bfbbc9f4ad2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :len1
+  attr_accessor :len2
+  attr_accessor :len3
+  attr_accessor :kind
+  
+  def initialize len1, len2, len3
+    @len1 = len1
+    @len2 = len2
+    @len3 = len3
+  end
+
+  def kind
+    if @len1 + @len2 <= @len3 || @len2 + @len3 <= @len1 || @len3 + @len1 <= @len2
+      raise TriangleError
+    elsif @len1 + @len2 + @len3 == 0
+      raise TriangleError
+    elsif @len1.negative?() == true || @len2.negative?() == true || @len3.negative?() == true
+      raise TriangleError
+    elsif @len1 == @len2 && @len3 == @len2
+      self.kind = :equilateral
+    elsif @len1 == @len2 || @len2 == @len3 || @len1 == @len3
+      self.kind = :isosceles
+    else 
+      self.kind = :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
